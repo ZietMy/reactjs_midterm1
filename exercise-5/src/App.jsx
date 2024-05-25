1c99ff7bd0ec716e3eaf90e20d60e9029668d996
Viet My <Ziettmy@gmail.com>
2024-05-25 10:43:34 +0700

    done ex5

--- exercise-5/src/App.jsx
+++ exercise-5/src/App.jsx
@@ -1,5 +1,18 @@
+import { LIST_USERS } from "./data.js";
+import Card from "./components/Card.jsx";
+
 function App() {
-  return <>{/* Your code  here */}</>;
+  return <>
+  <div class="cards-view">
+      <div class="cards-grid">
+  {
+    LIST_USERS.map((card,index)=>(
+      <Card key={index} card={card}/>
+    ))
+  }
+    </div>
+  </div>
+  </>;
 }
 
 export default App;
--- exercise-5/src/data.js
+++ exercise-5/src/data.js
@@ -1,1 +1,35 @@
-/* Your data here */
+import avt1 from "./assets/avt1.png";
+import avt2 from "./assets/avt2.png";
+import avt3 from "./assets/avt3.png";
+import avt4 from "./assets/avt4.png";
+
+export const LIST_USERS = [
+    {
+        id:"p1",
+        name:"Ding",
+        hobby:"Games",
+        address:"A",
+        image:avt1,    
+    },
+    {
+        id:"p2",
+        name:"Dang",
+        hobby:"Dances",
+        address:"B",
+        image:avt2,    
+    },
+    {
+        id:"p3",
+        name:"Dyng",
+        hobby:"Sing",
+        address:"E",
+        image:avt3,    
+    },
+    {
+        id:"p4",
+        name:"My",
+        hobby:"I like all things",
+        address:"A",
+        image:avt4,    
+    },
+];
